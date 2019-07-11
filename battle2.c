bfa594c52330dcf4d13596797ae13862ffc7d835
中島和真 <nakajikazuma@docomo.ne.jp>
2019-07-12 00:16:29 +0900

    first revision

---
+++ battle2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+char a[100];  
+int s,k,t;
+
+printf("What is your name?\n");
+  scanf("%s",a);
+  printf("Hello,%s\n",a);
+
+  srand(time(NULL));
+ 
+ s = rand()%6+1;
+ k = rand()%6+1;
+  printf("Rolling the dice...\n");
+ printf("Dice 1:%d\n",s);
+ printf("Dice 2:%d\n",k);
+ t = s+k;
+ printf("Total value :%d\n",t);
+
+ if(t >= 8){
+   printf("%s won!\n",a);
+ }
+ else printf("%s lost!\n",a);
+ 
+  return 0;
+}
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+  char a[100];
+  printf("What is your name?\n");
+  scanf("%s",a);
+  printf("Hello,%s\n",a);
+return 0;
+}
